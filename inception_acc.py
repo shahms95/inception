3b03bce9e5d215b419222e6f94c40120ff34e45d
Maulik <shahms95@gmail.com>
2019-05-08 23:03:13 -0500

    testing multi-gpu

--- inception_acc.py
+++ inception_acc.py
@@ -3,6 +3,7 @@ from keras import backend as K
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 from keras.applications.inception_v3 import InceptionV3
 from keras import optimizers
+from keras.utils import multi_gpu_model
 import numpy as np
 import CallBack
 import argparse
